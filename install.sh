546245bec2c523d130569e035fe9fd59ce175958
j03-dev <24nomeniavo@gmail.com>
2023-03-22 16:16:51 +0300

    update: Installation  to install all deps

--- install.sh
+++ install.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
-directory=$(pwd)
+DIRECTORY=$(pwd)
 
-sudo ln -s ${directory}/run.sh /bin/xerosploit
+# install deps
+sudo apt install nmap hping3 build-essential ruby-dev libpcap-dev libgmp3-dev
+
+# ruby deps for xettercap
+sudo gem install bettercap
+
+# install virtualenv
+sudo python3 -m pip install virtualenv
+
+# create virualenv
+python3 -m virtualenv .venv --python=2
+
+# active env
+source ${DIRECTORY}/.venv/bin/activate
+
+# install requirement
+python -m pip install tabulate terminaltables
+
+sudo ln -s ${DIRECTORY}/run.sh /bin/xerosploit
 echo [+] create link xerosploit
 
-xettercap="${directory}/tools/bettercap/bin/xettercap"
+XETTERCAP="${DIRECTORY}/tools/bettercap/bin/xettercap"
 
-sudo chmod +x $xettercap
+sudo chmod +x $XETTERCAP
 echo [+] make xettercap executable
 
-sudo ln -s ${xettercap} /bin/xettercap
+sudo ln -s ${XETTERCAP} /bin/xettercap
 echo [+] create xettercap link
 
 echo install finish
